2933c45e8c3a60aecf584ff1ee66533c70aa8c15
BenChang-collab <zxc110009@gmail.com>
2026-07-02 19:43:02 +0800

    docker build test

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
+import os
+import time
+import pymysql
 from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello Docker!"
+def get_db_connection():
+    while True:
+        try:
+            return pymysql.connect(
+                host=os.environ["DB_HOST"],
+                user=os.environ["DB_USER"],
+                password=os.environ["DB_PASSWORD"],
+                database=os.environ["DB_NAME"],
+                cursorclass=pymysql.cursors.DictCursor
+            )
+        except Exception as e:
+            print(f"資料庫連線中...: {e}")
+            time.sleep(2)
 
 @app.route("/health")
 def health():
